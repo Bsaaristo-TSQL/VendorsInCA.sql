f8326e4feae93e217646392f5543d3a499e26a1d
Bsaaristo <92605542+Bsaaristo@users.noreply.github.com>
2022-05-16 14:19:20 -0500

    Update Saaristo-Asg9-1-VendorsInCA.sql
    i will add the sql databases that these snippets use in either a README or a new project.

--- Saaristo-Asg9-1-VendorsInCA.sql
+++ Saaristo-Asg9-1-VendorsInCA.sql
@@ -2,3 +2,8 @@ SELECT VendorName, VendorState
 FROM     Vendors
 WHERE  (VendorState = 'CA')
 ORDER BY VendorState
+
+SELECT InvoiceID, VendorID, InvoiceDueDate, InvoiceNumber, PaymentDate, InvoiceDate, TermsID
+FROM     Invoices
+WHERE  (PaymentDate > CONVERT(DATETIME, '2016-04-01 00:00:00', 102))
+ORDER BY PaymentDate DESC
